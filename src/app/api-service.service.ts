b1a59abf4ff560ff761db84c8948ed6d2458af09
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-09-29 17:46:06 +0500

    update

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -9,7 +9,7 @@ import { throwError } from 'rxjs';
 export class ApiServiceService {
 
   constructor(private http: HttpClient) { }
-  apiUrl = 'http://localhost:3000/api/register';
+  apiUrl = 'http://www.localhost:3000/api/register';
 
   registerUser(formData: any){
     return this.http.post(this.apiUrl,formData, {responseType: 'json'}).pipe()
